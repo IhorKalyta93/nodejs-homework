6ed489283207af73fc0bea3bcbbc09e101991611
Ihor Kalyta <ihor.kalyta93@gmail.com>
2023-04-29 13:41:47 +0200

    1.2

--- middelwares/index.js
+++ middelwares/index.js
@@ -2,5 +2,5 @@ const ctrlWrapper = require('./ctrlWrapper');
 const validation = require('./validation');
 const notFound = require('./notFound');
 const globalErrorHandler = require('./globalErrorHandler');
-
+const auth = require('./auth');
 module.exports = { ctrlWrapper, validation, notFound, globalErrorHandler };
